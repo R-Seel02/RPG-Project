d3c7d1a6526fc0bfa2c1a2cc7dd6a41a96f0de9b
MarkP05 <mvpfister5@gmail.com>
2024-11-07 16:23:51 -0500

    small map changes

--- src/PickableObjects/itemAnchor.java
+++ src/PickableObjects/itemAnchor.java
@@ -1,18 +1,18 @@
-package PickableObjects;
+// package PickableObjects;
 
-import Engine.ImageLoader;
-import GameObject.Frame;
-import GameObject.SpriteSheet;
-import Level.PickableObject;
-import Utils.Point;
+// import Engine.ImageLoader;
+// import GameObject.Frame;
+// import GameObject.SpriteSheet;
+// import Level.PickableObject;
+// import Utils.Point;
 
-public class ItemAnchor extends PickableObject {
+// public class ItemAnchor extends PickableObject {
 
-    public ItemAnchor (Point location, Frame frame){
-        super(location, frame); new SpriteSheet(ImageLoader.load("Anchor.png"), 24, 24);
+//     public ItemAnchor (Point location, Frame frame){
+//         super(location, frame); new SpriteSheet(ImageLoader.load("Anchor.png"), 24, 24);
 
-    } 
+//     } 
 
   
- }
+//  }
 
